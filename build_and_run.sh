173b7d368f0cc667c4f37f23c0d89e93b80d7207
abergavenny <abergavenny@protonmail.ch>
2025-08-12 20:00:32 +0200

    Update build_and_run.sh

--- build_and_run.sh
+++ build_and_run.sh
@@ -15,12 +15,12 @@ mkdir -p "$OUT_DIR"
 echo "Compiling java"
 javac -h "$NATIVE_DIR" -d "$OUT_DIR" "$SRC_DIR/Main.java"
 
-# echo "Compiling native code to shared library"
+echo "Compiling native code to shared library"
 gcc -fPIC \
     -I"$JAVA_HOME/include" \
     -I"$JAVA_HOME/include/linux" \
     -shared -o "$OUT_DIR/$LIB_NAME" \
     "$NATIVE_DIR/BufferAddressJNI.cpp"
 
-# echo "Running Java program"
+echo "Running Java program"
 LD_LIBRARY_PATH=$OUT_DIR java -cp "$OUT_DIR" src.Main
